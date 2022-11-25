ca4056acb3e073d16820b3a1d2058412498fabb5
Faith Moraa <moraaf770@gmail.com>
2022-11-25 03:15:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+//multiply num1 and num 2, result should be 62
+
+const num1 = 31
+const num2 = 2
+let multiply = (num1 * num2)
+console.log(multiply) 
+
+let random = Math.floor(Math.random()*10)+1
+console.log(random)
+
+const num3 = 9;
+const num4 = 5;
+let mod = num3%num4;
+console.log(mod)
+
+let max = Math.max(4,15,20,3);
+console.log(max)
